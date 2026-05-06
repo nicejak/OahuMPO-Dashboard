03eea93b8b0f0abac866f9955c3e8853f5e45266
Alan Tong <atalantong@gmail.com>
2026-05-06 12:20:29 -1000

    update home mostly

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from numpy.random import default_rng as rng
+from make_plot import make_plot, make_chart_table
 
 st.set_page_config(layout="wide")
 st.logo("./images/logo-ompo-02.svg")
@@ -8,20 +8,42 @@ col1, col2 = st.columns(2, vertical_alignment = "center")
 col2.image("./images/Home_Header.png")
 col1.markdown("""
 # Welcome to the Oahu Regional Transportation Dashboard
-Find out how we are tracking our path towards regional transportation goals
+Track progress toward the adopted goals and performance measures of the ORTP 2050 through clear, data-driven reporting aligned with federal planning requirements.
 """)
 
 st.divider()
 
-st.markdown("# Explore performance measures that matter to you")
+st.markdown("## Explore performance measures that matter to you")
 
-col1, col2 = st.columns(2, vertical_alignment = "center", border = True)
+col1, col2 = st.columns(2, vertical_alignment = "center", border = False)
 
 col1.markdown("""
-    Over the coming months, community members will have opportunities to actively participate in shaping the vision and direction of the [Oahu Regional Transportation Plan 2050 (ORTP2050)](https://engage.oahumpo.org/oahu-regional-transportation-plan-ortp-2050)
+    This dashboard monitors system performance and trends to inform future ORTP updates, TIP programming, and performance-based investment decisions.
 """)
 
-col2.markdown("[Have questions? Let's hear them](mailto:oahumpo@oahumpo.org)")
+col2.link_button(":incoming_envelope: Have questions? Let's hear them", "mailto:oahumpo@oahumpo.org")
 
-st.markdown("# Featured measure")
-st.markdown("blah blah something")
+st.divider()
+
+col1, col2 = st.columns(2, vertical_alignment = "center", border = False)
+
+with col1:
+    make_plot(
+        path = "./data/safety/ompo/percent_of_crashes_involving_impaired_driving.csv",
+        title = "Percent of crashes involving impaired driving",
+        title_hover = "Focusing on reducing dangerous driving behaviors causing the highest number and proportion of crashes allows a targeted and efficient approach to improving overall safety. To target appropriate policies and programs to address dangerous driving behaviors, Oahu Metropolitan Planning Organization (OahuMPO) will monitor the percentage of crashes involving impaired driving using data provided by the State of Hawaii Department of Transportation.",
+        is_federal_measure = False,
+        source = "State of Hawaii Department of Transportation"
+    )
+
+col2.markdown("""
+    ### About this Measure
+
+    Focusing on reducing dangerous driving behaviors causing the highest number and proportion of crashes allows a targeted and efficient approach to improving overall safety.
+
+
+    To target appropriate policies and programs to address dangerous driving behaviors, Oahu Metropolitan Planning Organization (OahuMPO) will monitor the percentage of crashes involving impaired driving using data provided by the State of Hawaii Department of Transportation.
+
+    **Goal: Reduce serious injuries and traffic deaths to zero**
+""")
+col2.link_button("Learn More", "Safety")
